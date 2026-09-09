018bf74ae73be2f85f6632b83e001ec4fda69a09
Demis Bellot <demis.bellot@gmail.com>
2026-09-09 15:17:23 +0800

    Add GeneratedDtos StartupTask

--- MyApp/wwwroot/mjs/dtos.mjs
+++ MyApp/wwwroot/mjs/dtos.mjs
@@ -1,6 +1,6 @@
 /* Options:
-Date: 2023-11-16 13:19:05
-Version: 8.0
+Date: 2026-09-09 14:28:17
+Version: 10.15
 Tip: To override a DTO option, remove "//" prefix before updating
 BaseUrl: https://localhost:5001
 
@@ -13,42 +13,27 @@ BaseUrl: https://localhost:5001
 */
 
 "use strict";
-/** @typedef {'Mr'|'Mrs'|'Miss'} */
-export var Title;
-(function (Title) {
-    Title["Mr"] = "Mr"
-    Title["Mrs"] = "Mrs"
-    Title["Miss"] = "Miss"
-})(Title || (Title = {}));
-/** @typedef {'Action'|'Adventure'|'Comedy'|'Drama'} */
-export var FilmGenre;
-(function (FilmGenre) {
-    FilmGenre["Action"] = "Action"
-    FilmGenre["Adventure"] = "Adventure"
-    FilmGenre["Comedy"] = "Comedy"
-    FilmGenre["Drama"] = "Drama"
-})(FilmGenre || (FilmGenre = {}));
 export class QueryBase {
-    /** @param {{skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { Object.assign(this, init) }
     /** @type {?number} */
     skip;
     /** @type {?number} */
     take;
-    /** @type {string} */
+    /** @type {?string} */
     orderBy;
-    /** @type {string} */
+    /** @type {?string} */
     orderByDesc;
-    /** @type {string} */
+    /** @type {?string} */
     include;
-    /** @type {string} */
+    /** @type {?string} */
     fields;
-    /** @type {{ [index: string]: string; }} */
+    /** @type {?{ [index:string]: string; }} */
     meta;
 }
 /** @typedef T {any} */
 export class QueryDb extends QueryBase {
-    /** @param {{skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { super(init); Object.assign(this, init) }
 }
 export class AuditBase {
@@ -64,7 +49,7 @@ export class AuditBase {
     modifiedBy;
     /** @type {?string} */
     deletedDate;
-    /** @type {string} */
+    /** @type {?string} */
     deletedBy;
 }
 /** @typedef {'Single'|'Double'|'Queen'|'Twin'|'Suite'} */
@@ -114,26 +99,8 @@ export class Booking extends AuditBase {
     /** @type {?boolean} */
     cancelled;
 }
-export class Contact {
-    /** @param {{id?:number,userAuthId?:string,title?:Title,name?:string,color?:string,favoriteGenre?:FilmGenre,age?:number}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {number} */
-    id;
-    /** @type {string} */
-    userAuthId;
-    /** @type {Title} */
-    title;
-    /** @type {?string} */
-    name;
-    /** @type {?string} */
-    color;
-    /** @type {?FilmGenre} */
-    favoriteGenre;
-    /** @type {number} */
-    age;
-}
 export class ResponseError {
-    /** @param {{errorCode?:string,fieldName?:string,message?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{errorCode?:string,fieldName?:string,message?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { Object.assign(this, init) }
     /** @type {string} */
     errorCode;
@@ -141,42 +108,35 @@ export class ResponseError {
     fieldName;
     /** @type {string} */
     message;
-    /** @type {{ [index: string]: string; }} */
+    /** @type {?{ [index:string]: string; }} */
     meta;
 }
 export class ResponseStatus {
-    /** @param {{errorCode?:string,message?:string,stackTrace?:string,errors?:ResponseError[],meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{errorCode?:string,message?:string,stackTrace?:string,errors?:ResponseError[],meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { Object.assign(this, init) }
     /** @type {string} */
     errorCode;
-    /** @type {string} */
+    /** @type {?string} */
     message;
-    /** @type {string} */
+    /** @type {?string} */
     stackTrace;
-    /** @type {ResponseError[]} */
+    /** @type {?ResponseError[]} */
     errors;
-    /** @type {{ [index: string]: string; }} */
+    /** @type {?{ [index:string]: string; }} */
     meta;
 }
-export class GetContactsResponse {
-    /** @param {{results?:Contact[],responseStatus?:ResponseStatus}} [init] */
+/** @typedef T {any} */
+export class QueryResponse {
+    /** @param {{offset?:number,total?:number,results?:T[],meta?:{ [index:string]: string; },responseStatus?:ResponseStatus}} [init] */
     constructor(init) { Object.assign(this, init) }
-    /** @type {Contact[]} */
-    results;
-    /** @type {?ResponseStatus} */
-    responseStatus;
-}
-export class CreateContactResponse {
-    /** @param {{result?:Contact,responseStatus?:ResponseStatus}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {?Contact} */
-    result;
-    /** @type {?ResponseStatus} */
-    responseStatus;
-}
-export class UpdateContactResponse {
-    /** @param {{responseStatus?:ResponseStatus}} [init] */
-    constructor(init) { Object.assign(this, init) }
+    /** @type {number} */
+    offset;
+    /** @type {number} */
+    total;
+    /** @type {T[]} */
+    results = [];
+    /** @type {?{ [index:string]: string; }} */
+    meta;
     /** @type {?ResponseStatus} */
     responseStatus;
 }
@@ -186,130 +146,45 @@ export class HelloResponse {
     /** @type {string} */
     result;
 }
-export class RequiresAuthResponse {
-    /** @param {{result?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {string} */
-    result;
-}
-export class RequiresRoleResponse {
-    /** @param {{result?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {string} */
-    result;
-}
-export class RequiresAdminResponse {
-    /** @param {{result?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {string} */
-    result;
-}
 export class AuthenticateResponse {
-    /** @param {{userId?:string,sessionId?:string,userName?:string,displayName?:string,referrerUrl?:string,bearerToken?:string,refreshToken?:string,profileUrl?:string,roles?:string[],permissions?:string[],responseStatus?:ResponseStatus,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{userId?:string,sessionId?:string,userName?:string,displayName?:string,referrerUrl?:string,bearerToken?:string,refreshToken?:string,refreshTokenExpiry?:string,profileUrl?:string,roles?:string[],permissions?:string[],authProvider?:string,responseStatus?:ResponseStatus,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { Object.assign(this, init) }
-    /** @type {string} */
+    /** @type {?string} */
     userId;
-    /** @type {string} */
+    /** @type {?string} */
     sessionId;
-    /** @type {string} */
+    /** @type {?string} */
     userName;
-    /** @type {string} */
+    /** @type {?string} */
     displayName;
-    /** @type {string} */
+    /** @type {?string} */
     referrerUrl;
-    /** @type {string} */
+    /** @type {?string} */
     bearerToken;
-    /** @type {string} */
+    /** @type {?string} */
     refreshToken;
-    /** @type {string} */
+    /** @type {?string} */
+    refreshTokenExpiry;
+    /** @type {?string} */
     profileUrl;
-    /** @type {string[]} */
+    /** @type {?string[]} */
     roles;
-    /** @type {string[]} */
+    /** @type {?string[]} */
     permissions;
-    /** @type {ResponseStatus} */
+    /** @type {?string} */
+    authProvider;
+    /** @type {?ResponseStatus} */
     responseStatus;
-    /** @type {{ [index: string]: string; }} */
+    /** @type {?{ [index:string]: string; }} */
     meta;
-}
-/** @typedef T {any} */
-export class QueryResponse {
-    /** @param {{offset?:number,total?:number,results?:T[],meta?:{ [index: string]: string; },responseStatus?:ResponseStatus}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {number} */
-    offset;
-    /** @type {number} */
-    total;
-    /** @type {T[]} */
-    results;
-    /** @type {{ [index: string]: string; }} */
-    meta;
-    /** @type {ResponseStatus} */
-    responseStatus;
 }
 export class IdResponse {
     /** @param {{id?:string,responseStatus?:ResponseStatus}} [init] */
     constructor(init) { Object.assign(this, init) }
     /** @type {string} */
     id;
-    /** @type {ResponseStatus} */
+    /** @type {?ResponseStatus} */
     responseStatus;
-}
-export class GetContacts {
-    /** @param {{id?:number}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {?number} */
-    id;
-    getTypeName() { return 'GetContacts' }
-    getMethod() { return 'GET' }
-    createResponse() { return new GetContactsResponse() }
-}
-export class CreateContact {
-    /** @param {{title?:Title,name?:string,color?:string,favoriteGenre?:FilmGenre,age?:number,agree?:boolean}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {Title} */
-    title;
-    /** @type {string} */
-    name;
-    /** @type {?string} */
-    color;
-    /** @type {FilmGenre} */
-    favoriteGenre;
-    /** @type {number} */
-    age;
-    /** @type {boolean} */
-    agree;
-    getTypeName() { return 'CreateContact' }
-    getMethod() { return 'POST' }
-    createResponse() { return new CreateContactResponse() }
-}
-export class DeleteContact {
-    /** @param {{id?:number}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {number} */
-    id;
-    getTypeName() { return 'DeleteContact' }
-    getMethod() { return 'DELETE' }
-    createResponse() { }
-}
-export class UpdateContact {
-    /** @param {{id?:number,title?:Title,name?:string,color?:string,favoriteGenre?:FilmGenre,age?:number}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {number} */
-    id;
-    /** @type {?Title} */
-    title;
-    /** @type {?string} */
-    name;
-    /** @type {?string} */
-    color;
-    /** @type {?FilmGenre} */
-    favoriteGenre;
-    /** @type {?number} */
-    age;
-    getTypeName() { return 'UpdateContact' }
-    getMethod() { return 'PUT' }
-    createResponse() { return new UpdateContactResponse() }
 }
 export class Hello {
     /** @param {{name?:string}} [init] */
@@ -317,85 +192,38 @@ export class Hello {
     /** @type {?string} */
     name;
     getTypeName() { return 'Hello' }
-    getMethod() { return 'POST' }
+    getMethod() { return 'GET' }
     createResponse() { return new HelloResponse() }
 }
-export class RequiresAuth {
-    /** @param {{name?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {?string} */
-    name;
-    getTypeName() { return 'RequiresAuth' }
-    getMethod() { return 'POST' }
-    createResponse() { return new RequiresAuthResponse() }
-}
-export class RequiresRole {
-    /** @param {{name?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {?string} */
-    name;
-    getTypeName() { return 'RequiresRole' }
-    getMethod() { return 'POST' }
-    createResponse() { return new RequiresRoleResponse() }
-}
-export class RequiresAdmin {
-    /** @param {{name?:string}} [init] */
-    constructor(init) { Object.assign(this, init) }
-    /** @type {?string} */
-    name;
-    getTypeName() { return 'RequiresAdmin' }
-    getMethod() { return 'POST' }
-    createResponse() { return new RequiresAdminResponse() }
-}
 export class Authenticate {
-    /** @param {{provider?:string,state?:string,oauth_token?:string,oauth_verifier?:string,userName?:string,password?:string,rememberMe?:boolean,errorView?:string,nonce?:string,uri?:string,response?:string,qop?:string,nc?:string,cnonce?:string,accessToken?:string,accessTokenSecret?:string,scope?:string,returnUrl?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{provider?:string,userName?:string,password?:string,rememberMe?:boolean,accessToken?:string,accessTokenSecret?:string,returnUrl?:string,errorView?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { Object.assign(this, init) }
     /**
-     * @type {string}
+     * @type {?string}
      * @description AuthProvider, e.g. credentials */
     provider;
-    /** @type {string} */
-    state;
-    /** @type {string} */
-    oauth_token;
-    /** @type {string} */
-    oauth_verifier;
-    /** @type {string} */
+    /** @type {?string} */
     userName;
-    /** @type {string} */
+    /** @type {?string} */
     password;
     /** @type {?boolean} */
     rememberMe;
-    /** @type {string} */
-    errorView;
-    /** @type {string} */
-    nonce;
-    /** @type {string} */
-    uri;
-    /** @type {string} */
-    response;
-    /** @type {string} */
-    qop;
-    /** @type {string} */
-    nc;
-    /** @type {string} */
-    cnonce;
-    /** @type {string} */
+    /** @type {?string} */
     accessToken;
-    /** @type {string} */
+    /** @type {?string} */
     accessTokenSecret;
-    /** @type {string} */
-    scope;
-    /** @type {string} */
+    /** @type {?string} */
     returnUrl;
-    /** @type {{ [index: string]: string; }} */
+    /** @type {?string} */
+    errorView;
+    /** @type {?{ [index:string]: string; }} */
     meta;
     getTypeName() { return 'Authenticate' }
     getMethod() { return 'POST' }
     createResponse() { return new AuthenticateResponse() }
 }
 export class QueryBookings extends QueryDb {
-    /** @param {{id?:number,skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{id?:number,skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { super(init); Object.assign(this, init) }
     /** @type {?number} */
     id;
@@ -404,7 +232,7 @@ export class QueryBookings extends QueryDb {
     createResponse() { return new QueryResponse() }
 }
 export class QueryCoupons extends QueryDb {
-    /** @param {{id?:string,skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index: string]: string; }}} [init] */
+    /** @param {{id?:string,skip?:number,take?:number,orderBy?:string,orderByDesc?:string,include?:string,fields?:string,meta?:{ [index:string]: string; }}} [init] */
     constructor(init) { super(init); Object.assign(this, init) }
     /** @type {?string} */
     id;
